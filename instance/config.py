a753119c62197b1ad03a9ad87c91982e697eb36c
@balogun-abdurrahman <balogunabdurrahman101@gmail.com>
2025-11-30 02:21:10 +0100

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,10 @@
-DBPASS="123456"
-SECRET_KEY='eKI-C9p-Eyuc2g'
-SQLALCHEMY_DATABASE_URI="mysql+mysqlconnector://root@localhost/contact_gain"
-SQLALCHEMY_TRACK_MODIFICATIONS=True
+# DBPASS="123456"
+# SECRET_KEY='eKI-C9p-Eyuc2g'
+# SQLALCHEMY_DATABASE_URI="mysql+mysqlconnector://root@localhost/contact_gain"
+# SQLALCHEMY_TRACK_MODIFICATIONS=True
+
+MYSQL_HOST = "mysql://root:TOXdqIAyLwSvElXCAHKUjeqxbdHRreko@hopper.proxy.rlwy.net:32644/railway"
+MYSQL_USER = "railway"
+MYSQL_PASSWORD = "TOXdqIAyLwSvElXCAHKUjeqxbdHRreko"
+MYSQL_DB = "railway"
+MYSQL_PORT = 3306
